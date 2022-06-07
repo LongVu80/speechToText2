672aa917b61a7fc30f07f75f7fb28898fcd5627d
LongVu80 <thienlong14th@gmail.com>
2022-06-07 09:48:53 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,75 @@
+if ("webkitSpeechRecognition" in window) {
+  let speechRecognition = new webkitSpeechRecognition();
+  let final_transcript = "";
+
+  speechRecognition.continuous = true;
+  speechRecognition.interimResults = true;
+
+  speechRecognition.onstart = () => {
+    document.querySelector("#status").style.display = "block";
+  };
+  speechRecognition.onerror = () => {
+    document.querySelector("#status").innerHTML = `Speech Recognition Error`;
+    
+  };
+  speechRecognition.onend = () => {
+    document.querySelector("#status").innerHTML = `Speech Recognition is off due to no activity or Stop. Please press Start button again.`
+  };
+
+
+  speechRecognition.onresult = (event) => {
+    let interim_transcript = "";
+
+    for (let i = event.resultIndex; i < event.results.length; ++i) {
+      if (event.results[i].isFinal) {
+        final_transcript += event.results[i][0].transcript;
+      } 
+      else {
+        interim_transcript += event.results[i][0].transcript;
+      }
+    }
+    document.querySelector("#final").innerHTML = final_transcript + interim_transcript;
+  };
+
+  document.querySelector("#start").onclick = () => {
+    setTimeout(() => {
+      speechRecognition.start();
+    }, 50);
+    // this.openFullscreen()
+    document.querySelector("#status").innerHTML =`Voice Recognition is on`
+  };
+
+  document.querySelector("#stop").onclick = () => {
+    speechRecognition.stop();
+    document.querySelector("#status").innerHTML =`Press the Start Button`
+    // this.closeFullscreen()
+  };
+
+  document.querySelector("#clear").onclick = () => {
+    location.reload()
+  };
+} else {
+  console.log("Speech Recognition Not Available");
+}
+
+// var elem = document.documentElement;
+// function openFullscreen() {
+//   if (elem.requestFullscreen) {
+//     elem.requestFullscreen();
+//   } else if (elem.webkitRequestFullscreen) { /* Safari */
+//     elem.webkitRequestFullscreen();
+//   } else if (elem.msRequestFullscreen) { /* IE11 */
+//     elem.msRequestFullscreen();
+//   }
+// }
+
+// function closeFullscreen() {
+//   if (document.exitFullscreen) {
+//     document.exitFullscreen();
+//   } else if (document.webkitExitFullscreen) { /* Safari */
+//     document.webkitExitFullscreen();
+//   } else if (document.msExitFullscreen) { /* IE11 */
+//     document.msExitFullscreen();
+//   }
+// }
+
